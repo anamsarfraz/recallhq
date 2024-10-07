5d939f09e93eb18494da2daa186388401228a27f
Unum Sarfraz <anamsarfraz@gmail.com>
2024-10-06 20:09:25 -0700

    Integrate with video rag

--- video_processing/video_rag.py
+++ video_processing/video_rag.py
@@ -3,7 +3,8 @@ import json
 from glob import glob
 from llama_index.core.indices import MultiModalVectorStoreIndex
 
-from llama_index.core import SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.core.vector_stores.simple import SimpleVectorStore
 from llama_index.vector_stores.lancedb import LanceDBVectorStore
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 
@@ -26,12 +27,13 @@ class VideoRag:
     "Query: {query_str}\n"
     "Answer: "
     )
-    def __init__(self, data_path, storage_path = '', text_tsindex_dirpath = None, image_tsindex_dirpath = None, use_qdrant=True):
+    def __init__(self, data_path, storage_path = '', text_storage_path = '', text_tsindex_dirpath = None, image_tsindex_dirpath = None, use_qdrant=True):
         self.data_path = data_path
         self.use_qdrant = use_qdrant
         self.text_tsindex_dirpath = text_tsindex_dirpath
         self.image_tsindex_dirpath = image_tsindex_dirpath
         self.storage_path = storage_path
+        self.text_storage_path = text_storage_path
     
     def create_ts_index(self):
         if self.text_tsindex_dirpath:
@@ -64,10 +66,10 @@ class VideoRag:
         doc_store_path = os.path.join(self.storage_path, 'docstore.json')
         if os.path.exists(doc_store_path):
             print(f"Loading index from storage: {self.storage_path}")
-            self.storage_context = StorageContext.from_defaults(persist_dir=self.storage_path)
-            self.index = load_index_from_storage(self.storage_context)
+            storage_context = StorageContext.from_defaults(vector_store=self.text_store, image_store=self.image_store, persist_dir=self.storage_path)
+            self.index = load_index_from_storage(storage_context)
             if documents is not None:
-                self.add_documents(documents)
+                self.add_documents(self.index, self.storage_path, documents)
         else:
             # Create an empty vector store
             if not documents and os.path.exists(self.data_path):
@@ -80,18 +82,44 @@ class VideoRag:
             self.index = MultiModalVectorStoreIndex.from_documents(documents, storage_context=storage_context)
             self.index.storage_context.persist(persist_dir=self.storage_path)
 
+        if os.path.exists(self.text_storage_path):
+            print(f"Loading text index from storage: {self.text_storage_path}")
+            text_storage_context = StorageContext.from_defaults(persist_dir=self.text_storage_path)
+            self.text_index = load_index_from_storage(text_storage_context)
+            if documents is not None:
+                self.add_documents(self.text_index, self.text_storage_path, documents)
+        else:
+            if not documents and os.path.exists(self.data_path):
+                print(f"Creating a new text index from the data in {self.data_path}")
+                documents = SimpleDirectoryReader(input_dir=self.data_path, required_exts = [".txt"], recursive=True).load_data()
+            else:
+                documents = documents or []
+                print(f"Creating a new text index from the documents: {len(documents)}")
+            # Create an empty vector store
+            vector_store = SimpleVectorStore()
+            # Create a storage context with the empty vector store
+            text_storage_context = StorageContext.from_defaults(vector_store=vector_store)
+            # Create an empty VectorStoreIndex
+            self.text_index = VectorStoreIndex.from_documents(documents, storage_context=text_storage_context)
+            self.text_index.storage_context.persist(persist_dir=self.text_storage_path)
+
+
         self.retriever_engine = self.index.as_retriever(similarity_top_k=5, image_similarity_top_k=5)
+        self.text_retriever_engine = self.text_index.as_retriever(retrieval_mode='similarity', k=5)
 
-    def add_documents(self, documents):
-        self.index.refresh_ref_docs(documents)
-        self.index.storage_context.persist(persist_dir=self.storage_path)
+    def add_documents(self, index, storage_path, documents):
+        index.refresh_ref_docs(documents)
+        index.storage_context.persist(persist_dir=storage_path)
 
-    def add_document(self, document):
-        self.index.insert(document)
-        self.index.storage_context.persist(persist_dir=self.storage_path)
+    def add_document(self, index, storage_path, document):
+        index.insert(document)
+        index.storage_context.persist(persist_dir=storage_path)
 
     def count_documents(self):
         return len(self.index.vector_store.get_nodes())
+
+    def count_text_documents(self):
+        return len(self.text_index.docstore.docs)
 
     def print_text_tsindex(self):
         if self.text_tsindex:
@@ -145,4 +173,7 @@ class VideoRag:
 
         print(text_response.text)
         return text_response.text
+
+    def cleanup(self):
+        self.qdrant_client.close()
 
--- video_processing/video_rag_qdrant.py
+++ video_processing/video_rag_qdrant.py
@@ -1,6 +1,7 @@
 
-from llama_index.core.schema import ImageNode
 from .video_rag import VideoRag
+
+from llama_index.core.schema import ImageNode, ImageDocument
 
 
 class VideoRagQdrant(VideoRag):
@@ -14,9 +15,9 @@ class VideoRagQdrant(VideoRag):
     "Query: {query_str}\n"
     "Answer: "
     )
-    def __init__(self, data_path, storage_path = '', text_tsindex_dirpath = None, image_tsindex_dirpath = None, use_qdrant=True):
+    def __init__(self, data_path, storage_path = '', text_storage_path = '', text_tsindex_dirpath = None, image_tsindex_dirpath = None, use_qdrant=True):
         super().__init__(
-            data_path, storage_path = storage_path, text_tsindex_dirpath = text_tsindex_dirpath, image_tsindex_dirpath = image_tsindex_dirpath, use_qdrant=True)
+            data_path, storage_path = storage_path, text_storage_path = text_storage_path, text_tsindex_dirpath = text_tsindex_dirpath, image_tsindex_dirpath = image_tsindex_dirpath, use_qdrant=True)
     
     def retrieve_internal(self, retriever_engine, query_str):
         retrieval_results = retriever_engine.retrieve(query_str)
@@ -32,7 +33,40 @@ class VideoRagQdrant(VideoRag):
         return retrieved_image, retrieved_text 
 
     def retrieve(self, query_str):
-        return self.retrieve_internal(retriever_engine=self.retriever_engine, query_str=query_str)
+        img_docs = []
+        text_docs = []
+        img_nodes, text_nodes = self.retrieve_internal(retriever_engine=self.retriever_engine, query_str=query_str)
+
+
+        for node in img_nodes:
+            img_docs.append(ImageDocument(text=node.text, image_mimetype=node.image_mimetype, metadata=node.metadata))
+
+        for node in text_nodes:
+            text_docs.append(
+                {
+                    'text': node.text,
+                    'file_name': node.metadata['file_name'],
+                    'file_path': node.metadata['file_path'],
+                    'timestamps': node.metadata['timestamps']
+                }
+            )
+
+        text_only_results = self.text_retriever_engine.retrieve(query_str)
+        print(f"Text only retrieval results: {text_only_results}")
+
+        for node in text_only_results:
+            print(node)
+            print(node.metadata)
+            text_docs.append(
+                {
+                    'text': node.text,
+                    'file_name': node.metadata['file_name'],
+                    'file_path': node.metadata['file_path'],
+                    'timestamps': node.metadata['timestamps']
+                }
+            )
+
+        return img_docs, text_docs
         
     def query_with_oai(self, query_str, context, img_docs, event_metadata=""):
         text_response = self.openai_mm_llm.complete(prompt=VideoRag._query_prompt.format(
